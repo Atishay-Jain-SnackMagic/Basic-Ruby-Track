20412da54d3a5f629fe9a5bc18c5947b0b4bad9d
Atishay-Jain-SnackMagic <atishay.jain@snackmagic.com>
2025-01-13 20:40:27 +0530

    Update excercise2.rb

--- excercise2.rb
+++ excercise2.rb
@@ -1,8 +1,16 @@
-#!/usr/bin/env ruby
+# Solution to Basic Ruby Exercise 2 for replacing vowels with '*'
+class VowelReplacer
+  def initialize(input_val)
+    @input_val = input_val
+  end
 
+  def replace_vowel
+    @input_val = @input_val.gsub(/a|e|i|o|u/i, '*')
+  end
+end
 if ARGV.empty?
   puts 'Please provide an input'
 else
-  input = ARGV[0]
-  puts input.gsub(/a|e|i|o|u/, '*')
+  solution = VowelReplacer.new ARGV[0]
+  puts solution.replace_vowel
 end
