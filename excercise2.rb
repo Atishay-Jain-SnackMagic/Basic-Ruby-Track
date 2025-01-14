6ff0b63e222a84a35edf4bb4c500f45eee1470a8
Atishay-Jain-SnackMagic <atishay.jain@snackmagic.com>
2025-01-14 17:53:57 +0530

    Update excercise2.rb

--- excercise2.rb
+++ excercise2.rb
@@ -1,16 +1,18 @@
-# Solution to Basic Ruby Exercise 2 for replacing vowels with '*'
+# Implementing a class to replace the vowels with star
 class VowelReplacer
+  VOWEL_REGEX = /[aeiou]/i.freeze
   def initialize(input_val)
     @input_val = input_val
   end
 
-  def replace_vowel
-    @input_val = @input_val.gsub(/a|e|i|o|u/i, '*')
+  def replace_with_star
+    @input_val.gsub(VOWEL_REGEX, '*')
   end
 end
+
 if ARGV.empty?
   puts 'Please provide an input'
 else
-  solution = VowelReplacer.new ARGV[0]
-  puts solution.replace_vowel
+  replace_vowel_obj = VowelReplacer.new ARGV[0]
+  puts replace_vowel_obj.replace_with_star
 end
